0784db42c1c140380f7a118a7b3bc494c2faa530
philglenny24 <glennytronephilip@gmail.com>
2026-04-06 21:23:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Step 1: Global Variables
+
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+
+// Step 2: Function
+
+function addBurger() {
+  const newBurger = 'Flatburger'; // function-scoped
+  burgers.push(newBurger);
+}
+
+// Step 3: If Statement
+
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger'; // block-scoped
+  burgers.push(anotherNewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+}
